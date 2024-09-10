d50404a6cd837f6ce959498579fca001db3870ff
Абдуллаев Фарход <C:\Users\f.abdullaev\AppData\Roaming\The Bat!>
2024-09-10 16:52:13 +0500

    dashboard modify

--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -5,33 +5,9 @@ import Submenu from "./Submenu";
 import ReceiptIcon from "@mui/icons-material/Receipt";
 import CreditCardIcon from "@mui/icons-material/CreditCard";
 import Divider from "@mui/material/Divider";
-import { supabaseClient } from "../providers/supabase";
 
 export default function MainMenu() {
-  const [tableNamesData, setTableNamesData] = useState();
-  const dataProvider = useDataProvider();
   
-  
-  useEffect(() => {
-    dataProvider
-     .getMany("tables_name", {
-        pagination: { page: 1, perPage: 10 },
-        sort: { field: "id", order: "ASC" },
-      })
-     .then((responce) => {
-        console.log(responce.data[0].table_names);
-       setTableNamesData(responce.data);
-       console.log("tableNamesData",tableNamesData);
-       console.log("responce.data", responce.data);
-      })
-     .catch((error) => {
-        console.log(error);
-      });
-  }, []);
-  
-
-
-
 
   return (
     <Menu>
@@ -39,20 +15,17 @@ export default function MainMenu() {
       <Submenu text="Examples" icon={<ReceiptIcon />}>
         <Menu.ResourceItem name="posts" />
         <Menu.ResourceItem name="posters" />
+        <Menu.ResourceItem name="hse" />
+        <Menu.ResourceItem name="company_goals" />
+        <Menu.ResourceItem name="employees" />
       </Submenu>
       <Submenu text="Админка" icon={<CreditCardIcon />}>
         <Menu.ResourceItem name="groups" />
       </Submenu>
+      
       <Divider />
-      <Submenu text="tables_name">
-        {/* {tableNamesData.map((item) => { 
-          return (
-            <Menu.ResourceItem
-              key={item.id}
-              name={item.table_names} />
-          )
-        })} */}
-      </Submenu>
+      
+      
     </Menu>
   );
 }
--- src/pages/dashboard/Dashboard.jsx
+++ src/pages/dashboard/Dashboard.jsx
@@ -1,7 +1,47 @@
-import React from 'react'
+import React, { useState, useEffect, useMemo, useCallback } from "react";
+import { supabaseClient } from "../../providers/supabase";
 
 export default function Dashboard() {
+  const [tableNamesData, setTableNamesData] = useState([]);
+  const [list, setList] = useState();
+
+  // useEffect(() => {
+  //   async function fetchData() {
+  //     try {
+  //       const { data } = await supabaseClient.from("tables").select("*");
+  //       tableNamesData.push(data); // --> Рабочий вариант
+  //       // setTableNamesData((prev)=>prev.push(data)) // --> Рабочий вариант
+  //       console.log("tableNamesData", tableNamesData[0]);
+  //       console.log("tableNamesData", tableNamesData[0][1]);
+  //     } catch (error) {
+  //       console.log(error);
+  //     }
+  //   }
+  //   fetchData();
+  // }, []);
+
+  const fetch = useCallback(async () => {
+    try {
+      const { data } = await supabaseClient.from("tables").select("*");
+      tableNamesData.push(data); // --> Рабочий вариант
+      // setTableNamesData((prev)=>prev.push(data)) // --> Рабочий вариант
+      console.log("tableNamesData", tableNamesData[0]);
+      console.log("tableNamesData", tableNamesData[0][1]);
+    } catch (error) {
+      console.log(error);
+    }
+  }, [tableNamesData]);
+
+  useEffect(() => { 
+    fetch();
+  }, [fetch]);
+
   return (
-    <div>Dashboard</div>
-  )
+    <div>
+      <h1>Dashboard</h1>
+      <ul>
+        {tableNamesData[0].map(item => <li key={item.id}> {item.title} </li> )}
+      </ul>
+    </div>
+  );
 }
